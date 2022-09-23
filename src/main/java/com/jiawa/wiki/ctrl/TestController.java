696c1b820214d819697cbb8c41be7cdab4591184
xuezhuanfu <984462559.com>
2022-09-23 15:39:45 +0800

    3-4 集成mybatis

--- src/main/java/com/jiawa/wiki/ctrl/TestController.java
+++ src/main/java/com/jiawa/wiki/ctrl/TestController.java
@@ -1,14 +1,19 @@
 package com.jiawa.wiki.ctrl;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.hello}")
+    private String hello;
+
     @GetMapping(value = "test")
     public String helloWord(){
-        return "helloword!";
+        return "helloword!"+hello;
     }
 
     @PostMapping(value = "test/post")
