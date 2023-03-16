70646032d2116e57f0cd1b97711869aa6ad8af9d
xuezhuanfu <984462559.com>
2023-03-16 16:42:39 +0800

    5-2 使用axios实现数据跨域  5-3使用ref、reactive实现数据绑定

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,15 +1,76 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <a-button type="dashed">Dashed Button</a-button>
-    <a-button type="text">Text Button</a-button>
-  </div>
+  <a-layout>
+    <a-layout-sider width="200" style="background: #fff">
+      <a-menu
+          mode="inline"
+          v-model:selectedKeys="selectedKeys2"
+          v-model:openKeys="openKeys"
+          :style="{ height: '100%', borderRight: 0 }"
+      >
+        <a-sub-menu key="sub1">
+          <template #title>
+            <span><user-outlined />subnav 1</span>
+          </template>
+          <a-menu-item key="1">option1</a-menu-item>
+          <a-menu-item key="2">option2</a-menu-item>
+          <a-menu-item key="3">option3</a-menu-item>
+          <a-menu-item key="4">option4</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <template #title>
+            <span><laptop-outlined />subnav 2</span>
+          </template>
+          <a-menu-item key="5">option5</a-menu-item>
+          <a-menu-item key="6">option6</a-menu-item>
+          <a-menu-item key="7">option7</a-menu-item>
+          <a-menu-item key="8">option8</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub3">
+          <template #title>
+            <span><notification-outlined />subnav 3</span>
+          </template>
+          <a-menu-item key="9">option9</a-menu-item>
+          <a-menu-item key="10">option10</a-menu-item>
+          <a-menu-item key="11">option11</a-menu-item>
+          <a-menu-item key="12">option12</a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      Content
+      <pre>
+        {{ebooksRef[0]}}
+      </pre>
+      <pre>
+        {{ebooksReactive[0]}}
+      </pre>
+    </a-layout-content>
+  </a-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted, ref, reactive, toRef} from 'vue';
+import axios from 'axios';
 
 export default defineComponent({
-  name: 'Home'
+  name: 'Home',
+  setup(){
+    const ebooksRef = ref();
+    const ebooks2 = reactive({ebooksReactive:[]});
+    onMounted(()=>{
+      console.log("setup")
+      axios.get("http://localhost:8080/ebook/list?name=mysql").then((response)=>{
+        const data = response.data;
+        ebooksRef.value = data.content;
+        ebooks2.ebooksReactive = data.content;
+      })
+    })
+    return {
+      ebooksRef,
+      ebooksReactive: toRef(ebooks2,"ebooksReactive")
+    }
+  }
 });
 </script>
